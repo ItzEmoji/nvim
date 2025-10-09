c9ffdf3c9420755bd2a46b012278874d9a7c121c
ItzEmoji <no-reply@itzemoji.com>
2025-10-09 11:28:23 +0200

    upgraded to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,11 +3,16 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
     nvf.inputs.nixpkgs.follows = "nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { self, nixpkgs, nvf, ...}: {
-    packages."x86_64-linux".default = (nvf.lib.neovimConfiguration {
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      modules = [ ./nvf-configuration.nix ];
-    }).neovim;
+  outputs = { ... }@inputs:
+  inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
+    perSystem = { pkgs, ... }: {
+      packages.default = (inputs.nvf.lib.neovimConfiguration {
+        inherit pkgs;
+        modules = [ ./nvf-configuration.nix ];
+      }).neovim;
+    };
   };
 }
