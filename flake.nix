c418faa7a1cd33692c65be98d4561dc48817a377
ItzEmoji <cyril.dettling@gmail.com>
2026-01-25 15:53:58 +0000

    fixed caching

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "ItzEmoji's Neovim (nvf) – prebuilt via Cachix, nix run friendly";
+
   nixConfig = {
     substituters = [
       "https://itzemoji.cachix.org"
@@ -9,20 +11,41 @@
       "itzemoji.cachix.org-1:GHoBtGODGZYPwBZDJrbGDbOHRWq0Kn3TD3x5KC5NYZk="
     ];
   };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     nvf.url = "github:notashelf/nvf";
     nvf.inputs.nixpkgs.follows = "nixpkgs";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { ... }@inputs:
-  inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
-    perSystem = { pkgs, ... }: {
-      packages.default = (inputs.nvf.lib.neovimConfiguration {
-        inherit pkgs;
-        modules = [ ./nvf-configuration.nix ];
-      }).neovim;
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, nvf, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { system, pkgs, ... }:
+      let
+        nvimPackage =
+          (nvf.lib.neovimConfiguration {
+            inherit pkgs;
+            modules = [ ./nvf-configuration.nix ];
+          }).neovim;
+      in {
+        packages.default = nvimPackage;
+
+        apps.default = {
+          type = "app";
+          program = "${nvimPackage}/bin/nvim";
+        };
+      };
     };
-  };
 }
+
