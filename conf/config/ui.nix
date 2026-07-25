f44787c26dd248527664a1cca4d47eac8b63f8b7
ItzEmoji <no-reply@itzemoji.com>
2026-07-25 18:39:37 +0000

    feat(ui): highlight other uses of the word under the cursor
    Adds vim-illuminate, which works off treesitter and regex, so it does
    not depend on a language server being enabled.

--- conf/config/ui.nix
+++ conf/config/ui.nix
@@ -3,6 +3,9 @@
   vim.ui = {
     noice.enable = true;
     colorizer.enable = true;
+
+    # Highlight the other occurrences of whatever word the cursor is on.
+    illuminate.enable = true;
   };
   vim.visuals.nvim-web-devicons.enable = true;
 }
